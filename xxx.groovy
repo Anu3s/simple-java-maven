010b028f1b96bf6820883ff22575d190afccb64a
Anu3s <anusha3job@gmail.com>
2022-08-08 21:59:53 +0530

    saving6

--- xxx.groovy
+++ xxx.groovy
@@ -1,15 +1,16 @@
-job('seed_joblocal') {
-    description("lets try groovy for first time")
-    scm {
-         git("git@github.com:Anu3s/simple-java-maven.git", master)
-    } 
-    triggers {
-          scm('* * * * *')
-    }
-    steps {
-        maven('clean package','simple-java-maven/pom.xml')
-    }
-    publishers {
-        archieveArtifacts '**/*.jar'
-    }
-}
+job("scriptjob") {
+   description("lets solve this")
+   scm {
+       git("git@github.com:Anu3s/simple-java-maven", master)
+   }
+   triggers {
+       scm(* * * * *)
+   }
+   steps {
+       maven('clean package', 'simple-java-maven/pom.xml')
+
+   }
+   publishers {
+       archieveArtifacts '**/*.jar'
+
+   }
