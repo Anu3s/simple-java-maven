3ae1292d4f9ba66bfa02f8ed58fe0572d3548a17
Anu3s <anusha3job@gmail.com>
2022-08-08 20:53:20 +0530

    for groovy

---
+++ xxx.groovy
@@ -0,0 +1,15 @@
+job('seed_job') {
+    description("lets try groovy for first time on $DATE")
+    scm {
+         git("https://github.com/Anu3s/simple-java-maven.git,master)
+    } 
+    triggers {
+          scm('* * * * *')
+    }
+    steps {
+        maven('clean package' ,'https://github.com/Anu3s/simple-java-maven/blob/master/pom.xml')
+    }
+    publishers {
+        archieveArtifacts '**/*.jar'
+    }
+}
